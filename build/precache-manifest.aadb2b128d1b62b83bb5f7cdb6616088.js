4683a1042cb29f7833ffda882e0c741886608d56
bautheac <o.bautheac@gmail.com>
2020-03-24 18:13:18 +0000

    Deploying to github pages

---
+++ build/precache-manifest.aadb2b128d1b62b83bb5f7cdb6616088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8786286941840ecb5a89ac0d99c1101c",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "1351a3a5846c74abe62d",
+    "url": "/smartbrain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7d7df1b1a3d69f063f45",
+    "url": "/smartbrain/static/css/main.d626af46.chunk.css"
+  },
+  {
+    "revision": "1351a3a5846c74abe62d",
+    "url": "/smartbrain/static/js/2.5b0b637a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/smartbrain/static/js/2.5b0b637a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d7df1b1a3d69f063f45",
+    "url": "/smartbrain/static/js/main.26a89279.chunk.js"
+  },
+  {
+    "revision": "62f6ac12727a586f9301",
+    "url": "/smartbrain/static/js/runtime-main.24fa6657.js"
+  }
+]);
